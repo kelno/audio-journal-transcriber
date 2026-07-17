317a6c8964f1a0e10652622c2773de09b0f25652
Kelno <kelnowm@gmail.com>
2026-07-17 22:47:15 +0200

    Update text for MULTIPLE_TRANSCRIPTS_SEPARATOR
    To play better with Obsidian which understood as a markdown link

--- src/transcriber/constants.py
+++ src/transcriber/constants.py
@@ -4,4 +4,4 @@ COMMANDS_FILENAME = "_commands.md"
 METADATA_FILENAME = "_metadata.md"
 DELETED_DIR_NAME = "_deleted"
 
-MULTIPLE_TRANSCRIPTS_SEPARATOR = "\n\n[next transcripted audio]\n\n"
+MULTIPLE_TRANSCRIPTS_SEPARATOR = "\n\n<next transcripted audio follows>\n\n"
